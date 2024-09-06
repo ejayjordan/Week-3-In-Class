365a7af31b68011f26e407c576c383e72357400e
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2024-09-06 11:45:59 -0400

    fix boolean

--- java.js
+++ java.js
@@ -1,18 +1,18 @@
 let txtPassword = document.getElementById("password")
 var specialChars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=";
 var numbers = "0123456789"
-Boolean(false)
+let passwordCheck = false
 
 function testWord(){
     
     if(txtPassword != null && txtPassword.length >= 8){
         if(specialChars.test(txtPassword)){
             if(numbers.test(txtPassword)){
-               Boolean(true)
+                passwordCheck = true
             }
         }
     }
-    if(true){
+    if(passwordCheck = true){
         console.log("You got it!")
         document.getElementById("password").style.borderColor = '1px solid green'
     }
