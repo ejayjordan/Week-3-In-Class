a44f8b3966f47564462d55931d78c4ead1152a9b
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2024-09-06 11:38:37 -0400

    Added checks

--- java.js
+++ java.js
@@ -1,14 +1,23 @@
 let txtPassword = document.getElementById("password")
 var specialChars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=";
+var numbers = "0123456789"
+Boolean(false)
 
 function testWord(){
     
     if(txtPassword != null && txtPassword.length >= 8){
-        if(){
-            if(){
-               
+        if(specialChars.test(txtPassword)){
+            if(numbers.test(txtPassword)){
+               Boolean(true)
             }
         }
     }
-    
+    if(true){
+        console.log("You got it!")
+        document.getElementById("password").style.borderColor = '1px solid green'
+    }
+    else{
+        console.log("Absolutely not")
+        document.getElementById("password").style.borderColor = '1px solid red'
+    }
 }
